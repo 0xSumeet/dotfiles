e7cf47dba39e0d31a7542b89e2c617b352dc4766
0xSumeet <techie.sam.dev.400@gmail.com>
2024-04-18 09:52:10 +0530

    vim commands

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,10 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set noswapfile")
+vim.cmd("set relativenumber")
+vim.cmd("set mouse=a")
+vim.g.mapleader = " "
+vim.cmd("nnoremap <leader>v :vsplit<CR>")
+vim.cmd("nnoremap <leader>h :split<CR>")
