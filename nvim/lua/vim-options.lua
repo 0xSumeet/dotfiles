0b5011eb6aef485ec02e2eb0539e787cc6ab0ae7
0xSumeet <techie.sam.dev.400@gmail.com>
2024-09-15 17:51:47 +0530

    deleted:    lua/plugins.lua 	modified:   lua/vim-options.lua

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -6,6 +6,15 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set noswapfile")
 vim.cmd("set relativenumber")
 vim.cmd("set mouse=a")
+
 vim.g.mapleader = " "
-vim.cmd("nnoremap <leader>v :vsplit<CR>")
+vim.cmd("nnoremap <leader>v :vsplit<CR>") --<CR> is Enter here
 vim.cmd("nnoremap <leader>h :split<CR>")
+--vim.cmd("nnoremap <leader><CR> :Vex<CR>")
+-- Use Ctrl + w + ] to nvaigate to the function and Ctrl + w + q to close that window.`
+
+--use Visual Line to select line and more the line using J to up or K to down
+vim.cmd("vnoremap K :m '<-2<CR>gv=gv")
+vim.cmd("vnoremap J :m '>+1<CR>gv=gv")
+
+
