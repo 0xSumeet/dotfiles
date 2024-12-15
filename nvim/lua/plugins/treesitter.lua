edd813ddb882bb6b5f44b0273b3cd3946c105568
Sumeet Patel <0xSumeet@users.noreply.github.com>
2024-12-15 22:54:11 +0530

    modified:   nvim/lua/plugins/treesitter.lua

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,15 @@
 -- Plugin for higlighting and indenting the code, for indenting the code, select the code and press =
 
 return {
-  "nvim-treesitter/nvim-treesitter", 
-  build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			auto_install = true,
+			ensure_installed = { "go", "lua", "vim", "bash", "markdown", "markdown_inline" }, --use :TSInstallInfo to view all options and use :TSInstall bash to install it.
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
